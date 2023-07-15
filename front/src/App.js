aad2105f585943a0992048b82bbe4996cb02df51
Maxik <mpoplavskiy38380@gmail.com>
2023-07-15 13:03:10 +0300

    Create home page

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from "./components/navbar";
 import Footer from './components/footer';
+import Home from './components/home';
+import { Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
+    <globalThemeContext.Provider value={globalTheme}>
     <div className="App">
       <Navbar />
+        
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        
       <Footer />
     </div>
+    </globalThemeContext.Provider>
   );
 }
 
---
+++ front/src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+function Home(props)
+{
+    return <div class="content">
+        <h1 className="title">MAXFORUM<hr /></h1>
+        <p className="about">MaxForum is a popular online discussion platform that has gained significant traction among internet users in recent years. With its user-friendly interface and robust features, MaxForum has become a go-to destination for individuals and communities looking to engage in meaningful conversations, share knowledge, and connect with like-minded individuals.</p>
+    </div>
+}
+
+export default Home;
