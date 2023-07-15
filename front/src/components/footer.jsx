0559445dfe875d4cb4303b52bbd87e2465d87c90
Maxik <mpoplavskiy38380@gmail.com>
2023-07-15 11:48:51 +0300

    create footer component

---
+++ front/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./footer.css";
+
+function Footer(props)
+{
+    return <div className="footer">
+        <h3 className="footer-text">Maxik incorporater©2023</h3>
+    </div>
+}
+
+export default Footer;
