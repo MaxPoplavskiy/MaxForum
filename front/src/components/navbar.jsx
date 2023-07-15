6bdabdaf5e19bb9c53568161f4e3dca05a12b779
Maxik <mpoplavskiy38380@gmail.com>
2023-07-15 11:37:06 +0300

    create navbar component

--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -4,11 +4,17 @@ import "./navbar.css";
 function Navbar(props)
 {
     return <div id="navbar">
-        <ul id="navbar-list">
-            <div id="navbar-logo" className="navbar-item">MaxForum</div>
+        
+        <div className="navbar-list">
+            <div id="navbar-logo">MaxForum</div>
             <div className="navbar-item">Posts</div>
             <div className="navbar-item">Create</div>
-        </ul>
+        </div>
+        <div className="navbar-list">
+            
+            <div className="navbar-item">My Posts</div>
+            <div className="navbar-item">Account</div>
+        </div>
     </div>
 }
 
