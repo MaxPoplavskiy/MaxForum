d374c2e0ee97712c9e3e725d833480af385046ef
Maxik <mpoplavskiy38380@gmail.com>
2023-07-16 18:34:15 +0300

    create Create component with dark and light theme

--- front/src/components/create-post.jsx
+++ front/src/components/create-post.jsx
@@ -10,6 +10,24 @@ function CreatePost(props)
 
     return <div className={cssLightHandle("create-menu", theme)}>
         <h1 className={cssLightHandle("title", theme)}>Create<hr className={cssLightHandle("", theme)} /></h1>
+        <div className="data-forms-container">
+            <div className="img-form-container">
+                <h2 className={cssLightHandle("title-form", theme)}>Image<hr className={cssLightHandle("thick-h2", theme)} /></h2>
+                <div className={cssLightHandle("button transperent-button img-upload-div", theme)}>
+                <label for="file-upload" class={cssLightHandle(" img-upload", theme)}>
+                    Upload Image
+                </label>
+                <input id="file-upload" type="file" />
+                </div>
+            </div>
+            <div className="text-form-container">
+                <h2 className={cssLightHandle("title-form", theme)}>Title<hr className={cssLightHandle("thick-h2", theme)} /></h2>
+                <input className={cssLightHandle("title-input", theme)}></input>
+
+                <h2 className={cssLightHandle("title-form", theme)}>Content<hr className={cssLightHandle("thick-h2", theme)} /></h2>
+                <div className={cssLightHandle("content-input-div", theme)}><textarea className={cssLightHandle("content-input", theme)}></textarea></div>
+            </div>
+        </div>
     </div>
 }
 
