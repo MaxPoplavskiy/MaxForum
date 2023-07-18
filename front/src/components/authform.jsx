25f59263e9aa6856a72573454fb8379a8f5f97a8
Maxik <mpoplavskiy38380@gmail.com>
2023-07-18 10:35:14 +0300

    add wrong password repeat animation and changed style in authform

--- front/src/components/authform.jsx
+++ front/src/components/authform.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./authform.css";
 import { globalThemeContext, cssLightHandle } from "../globalThemeContext";
 import { useContext } from "react";
@@ -8,13 +8,42 @@ function AuthForm(props)
 {
     const theme = useContext(globalThemeContext);
 
+    const [firstPassword, setFirstPassword] = useState("");
+    const [secondPassword, setSecondPassword] = useState("");
+
+    function checkPasswordsEquality(pas1, pas2)
+    {
+        if(pas1 === "" || pas2 === "")
+        {
+            return "";
+        }
+        if(pas1 !== pas2)
+        {
+            return "red";
+        }
+        else
+        {
+            return "green";
+        }
+    }
+
+    function firstPasswordChange(event)
+    {
+        setFirstPassword(event.target.value);
+    }
+
+    function secondPasswordChange(event)
+    {
+        setSecondPassword(event.target.value);
+    }
+
     return <div className={cssLightHandle("content", theme)}>
         <div className={cssLightHandle("auth-container", theme)}>
             <div><h1 id="auth-title" className={cssLightHandle("title", theme)}>{props.title}</h1></div>
             <div className={cssLightHandle("auth-inputs-container", theme)}>
                 <input type="email" className={cssLightHandle("authform-input", theme)} placeholder="Email" />
-                <input type="password" className={cssLightHandle("authform-input", theme)} placeholder="Password" />
-                {props.title === "Register" ? <input type="password" className={cssLightHandle("authform-input", theme)} placeholder="Repeat password" /> : ""}
+                <input type="password" style={{borderColor: checkPasswordsEquality(firstPassword, secondPassword)}} className={cssLightHandle("authform-input", theme)} placeholder="Password" onChange={firstPasswordChange} />
+                {props.title === "Register" ? <input type="password" style={{borderColor: checkPasswordsEquality(firstPassword, secondPassword)}} className={cssLightHandle("authform-input", theme)} placeholder="Repeat password" onChange={secondPasswordChange} /> : ""}
             </div>
             <button id="authform-submit-btn" className={cssLightHandle("button transperent-button", theme)}>Submit</button>
         </div>
