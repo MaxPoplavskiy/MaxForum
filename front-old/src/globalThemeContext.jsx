520f853253f979c416b1d410a240f18a1ddff7a0
Maxim Poplavskiy <maxim.pop@cgsteam.io>
2024-11-17 18:21:20 +0200

    feat: Remove old server and front

--- front-old/src/globalThemeContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-function cssLightHandle(classname, theme)
-{
-    return classname + (theme === "light" ? " light" : "")
-}
-
-export { cssLightHandle };
-export const globalThemeContext = createContext("dark");
