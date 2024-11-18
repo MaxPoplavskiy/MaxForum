e1baa37ae019c49d783a4e34f6dda7a82dd94f80
Maxim Poplavskiy <maxim.pop@cgsteam.io>
2024-11-18 15:15:11 +0200

    feat: Add docker

--- front/src/common/hooks/use-switch-to-default-chain.hook.ts
+++ front/src/common/hooks/use-switch-to-default-chain.hook.ts
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 import { RpcError } from "viem";
-import { hardhat } from "viem/chains";
+import { sepolia } from "viem/chains";
 import { useAccount } from "wagmi";
 
 type Return = {
@@ -10,13 +10,13 @@ export const useSwitchToDefaultChain = (): Return => {
   const { chain } = useAccount();
 
   const switchToDefaultChain = useCallback(async () => {
-    if (chain?.id === hardhat.id) return;
+    if (chain?.id === sepolia.id) return;
     try {
       await window.ethereum.request({
         method: "wallet_switchEthereumChain",
         params: [
           {
-            chainId: '0x7A69',
+            chainId: '0xAA36A7',
           },
         ],
       });
@@ -27,13 +27,14 @@ export const useSwitchToDefaultChain = (): Return => {
             method: "wallet_addEthereumChain",
             params: [
               {
-                chainId: '0x7A69',
-                chainName: hardhat.name,
+                chainId: '0xAA36A7',
+                chainName: sepolia.name,
                 nativeCurrency: {
-                  name: "ETH",
+                  name: "Sepolia ETH",
                   symbol: "ETH",
                   decimals: 18,
                 },
+                rpcUrls: ["https://rpc.sepolia.org"],
               },
             ],
           });
